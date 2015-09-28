5eeb2df0c73262d88d4be31d84fd83e343eecb7b
Fred Damstra <fred.damstra@gmail.com>
2015-09-28 17:23:51 -0400

    First draft of script, added vimrc, slight update to README.

---
+++ CreateLinks.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+#
+# Creates links.
+PWD=`pwd`
+for file in .bashrc .vimrc
+do
+  if [[ -e ~/$file ]]
+  then
+    echo Warning! ~$file already exists. Renaming to .old
+    mv ~/$file ~/$file.old
+  fi
+  ln -s $PWD/$file ~/$file
+done
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MyEnvironment
-My environment scripts (bash, vim, etc)
+These are my environment scripts (bash, vim, etc).
+
+Also, my method of refreshing my skills on a couple things. Really, nothing to see here.
